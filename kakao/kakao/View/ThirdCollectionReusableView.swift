39cf2ba67ff1d120b8f22c8a6681aea403eb34c5
mcrkgus <mcrkgus@naver.com>
2024-01-04 04:19:16 +0900

    [Feat] Section 별 Header

---
+++ kakao/kakao/View/ThirdCollectionReusableView.swift
@@ -0,0 +1,12 @@
+//
+//  ThirdCollectionReusableView.swift
+//  kakao
+//
+//  Created by Gahyun Kim on 2024/01/03.
+//
+
+import UIKit
+
+class ThirdCollectionReusableView: UICollectionReusableView {
+        
+}
